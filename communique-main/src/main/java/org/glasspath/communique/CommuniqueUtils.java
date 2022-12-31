b441ea437a7847673cfd0813b55b5e07f3392b20
remcopoelstra <remco@poelstrabesturingen.nl>
2022-12-31 15:02:37 +0100

    Updated temp dir location

--- communique-main/src/main/java/org/glasspath/communique/CommuniqueUtils.java
+++ communique-main/src/main/java/org/glasspath/communique/CommuniqueUtils.java
@@ -34,7 +34,7 @@ public class CommuniqueUtils {
 
 	public static File getTempDir() {
 
-		File tempDir = new File(System.getProperty("user.home") + "/.revenue/log"); //$NON-NLS-1$ //$NON-NLS-2$
+		File tempDir = new File(System.getProperty("user.home") + "/.revenue/temp"); //$NON-NLS-1$ //$NON-NLS-2$
 
 		if (!tempDir.exists()) {
 			try {
